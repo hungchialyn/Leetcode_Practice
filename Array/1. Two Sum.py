aea33c489d77e7152a263ff3cbaf7b10a7bc1945
hungchialyn <hungchialyn@gmail.com>
2026-04-17 21:43:11 +0800

    complement

--- Array/1. Two Sum.py
+++ Array/1. Two Sum.py
@@ -12,3 +12,18 @@ class Solution(object):
                 if (element1+element2) ==target:
                     return[i,j] 
         
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        m={}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in m:
+                return [m[complement], i]
+            m[num] = i
+        
