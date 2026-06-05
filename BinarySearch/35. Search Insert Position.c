e9e0ae0d167b06de488b5ad2f0f0439efaf4dbfa
hungchialyn <hungchialyn@gmail.com>
2026-06-05 23:45:05 +0800

    easy

--- BinarySearch/35. Search Insert Position.c
+++ BinarySearch/35. Search Insert Position.c
@@ -20,3 +20,24 @@ int searchInsert(int* nums, int numsSize, int target) {
     }
     return left+1;
 }
+
+//20260605
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        if(nums[mid] < target){
+            left = mid + 1;
+        }
+        if(nums[mid] > target){
+            right = mid - 1;
+        }
+    }
+    
+    return left;
+}
